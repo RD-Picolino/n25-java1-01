9100c53980eff4c03762429d2a2b457040a530cd
Rodrigo Picoloto <craftpicolino@gmail.com>
2025-06-24 21:48:20 -0300

    Conteudo da aula 08 ECLIPSE

--- Eclipse/WorkSpace/n25-java1-01/src/br/com/senaisp/bauru/aula07/Carta.java
+++ Eclipse/WorkSpace/n25-java1-01/src/br/com/senaisp/bauru/aula07/Carta.java
@@ -45,4 +45,17 @@ public class Carta {
 	private void setValor(int valor) {
 		this.valor = valor;
 	}
+	@Override
+	public String toString() {
+		String ret ="┌────┐\n"
+				  + "│#  │\n"
+				  + "│ !  │\n"
+				  + "│  #│\n"
+				  + "└────┘\n";
+		String ap = (getNumero().equals("10") ? "" : " ");
+		ret = ret.replaceFirst("#", getNumero() + ap);
+		ret = ret.replaceAll("!", getNaipe());
+		ret = ret.replaceFirst("#", ap + getNumero());
+		return ret;
+	}
 }
--- Eclipse/WorkSpace/n25-java1-01/src/br/com/senaisp/bauru/aula07/CartaTeste.java
+++ Eclipse/WorkSpace/n25-java1-01/src/br/com/senaisp/bauru/aula07/CartaTeste.java
@@ -10,6 +10,7 @@ public class CartaTeste {
 		System.out.println(ct.getNaipe());
 		System.out.println(ct.getNumero());
 		System.out.println(ct.getValor());
+		System.out.println(ct);
 	}
 
 }
